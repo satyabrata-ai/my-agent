3956db4a98877e51e61b53b87c1a8e2e32eb36a1
Satyabrata Saha <satyabratasaha20@gmail.com>
2025-12-14 16:49:58 +0530

    Test

--- app/agent.py
+++ app/agent.py
@@ -42,7 +42,7 @@ def get_weather(query: str) -> str:
     """
     if "sf" in query.lower() or "san francisco" in query.lower():
         return "It's 60 degrees and foggy."
-    return "It's 90 degrees and sunny."
+    return "It's 90 degrees and sunny. "
 
 
 def get_current_time(query: str) -> str:
